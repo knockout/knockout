194341f5ed3790cd9984235d48ad697d3e2b6452
Steve Sanderson <steve@codeville.net>
2010-07-08 19:27:04 +0100

    Shelving work on gridEditor example

--- spec/defaultBindingsBehaviors.js
+++ spec/defaultBindingsBehaviors.js
@@ -253,3 +253,16 @@ describe('Binding: CSS rule', {
         value_of(testNode.childNodes[0].className).should_be("unrelatedClass1 unrelatedClass2 myRule");
     }
 });
+
+describe('Binding: Unique Name', {
+    before_each: prepareTestNode,
+
+    'Should apply a different name to each element': function () {
+        testNode.innerHTML = "<div data-bind='uniqueName: true'></div><div data-bind='uniqueName: true'></div>";
+        ko.applyBindings(testNode, { });
+
+        value_of(testNode.childNodes[0].name.length > 0).should_be(true);
+        value_of(testNode.childNodes[1].name.length > 0).should_be(true);
+        value_of(testNode.childNodes[0].name == testNode.childNodes[1].name).should_be(false);
+    }
+});
