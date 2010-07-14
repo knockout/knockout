08b883a5de0929bb1d6c5fc13b0b4c0226cbd651
Steve Sanderson <steve@codeville.net>
2010-07-14 10:21:18 +0100

    Bumping up the version number

--- build/version-header.js
+++ build/version-header.js
@@ -1,4 +1,4 @@
-// Knockout JavaScript library v1.01
+// Knockout JavaScript library v1.02
 // (c) 2010 Steven Sanderson - http://knockoutjs.com/
 // License: Ms-Pl (http://www.opensource.org/licenses/ms-pl.html)
 
