9d2125021eea7cbc14d87a880a580850772c1d51
Steve Sanderson <steve@codeville.net>
2010-10-15 12:28:57 +0100

    Support for newer version of jquery-tmpl as well as older one.

--- src/templating/jquery.tmpl/jqueryTmplTemplateEngine.js
+++ src/templating/jquery.tmpl/jqueryTmplTemplateEngine.js
@@ -1,6 +1,19 @@
 /// <reference path="../templating.js" />
 
 ko.jqueryTmplTemplateEngine = function () {
+    // Detect which version of jquery-tmpl you're using. Unfortunately jquery-tmpl 
+    // doesn't expose a version number, so we have to infer it.
+    this.jQueryTmplVersion = (function() {
+        var missingDependenciesError = "To use KO's default template engine, reference jQuery and jquery-tmpl. See Knockout installation documentation for more details.";
+        if (typeof(jQuery) == "undefined")
+            throw new Error("jQuery not detected.\n" + missingDependenciesError);
+        if (!jQuery.tmpl)
+            throw new Error("jquery.tmpl not detected.\n" + missingDependenciesError);
+        if (jQuery.tmpl.tag)
+            return 2; // Since it exposes no official version number, we use our own numbering system. To be updated as jquery-tmpl evolves.
+        return 1;
+    })();
+
     function getTemplateNode(template) {
         var templateNode = document.getElementById(template);
         if (templateNode == null)
@@ -8,17 +21,26 @@ ko.jqueryTmplTemplateEngine = function () {
         return templateNode;
     }
 
-	var aposMarker = "__ko_apos__";
-	var aposRegex = new RegExp(aposMarker, "g");
+    // These two only needed for jquery-tmpl v1
+    var aposMarker = "__ko_apos__";
+    var aposRegex = new RegExp(aposMarker, "g");
+    
     this.renderTemplate = function (template, data, options) {
-        // jquery.tmpl doesn't like it if the template returns just text content or nothing - it only likes you to return DOM nodes.
-        // To make things more flexible, we can wrap the whole template in a <script> node so that jquery.tmpl just processes it as
-        // text and doesn't try to parse the output. Then, since jquery.tmpl has jQuery as a dependency anyway, we can use jQuery to
-        // parse that text into a document fragment using jQuery.clean().        
-        var templateTextInWrapper = "<script type=\"text/html\">" + getTemplateNode(template).text + "</script>";
-        var renderedMarkupInWrapper = $.tmpl(templateTextInWrapper, data);
-        var renderedMarkup = renderedMarkupInWrapper[0].text.replace(aposRegex, "'");
-        return jQuery.clean([renderedMarkup], document);
+        if (this.jQueryTmplVersion == 1) {    	
+            // jquery.tmpl v1 doesn't like it if the template returns just text content or nothing - it only likes you to return DOM nodes.
+            // To make things more flexible, we can wrap the whole template in a <script> node so that jquery.tmpl just processes it as
+            // text and doesn't try to parse the output. Then, since jquery.tmpl has jQuery as a dependency anyway, we can use jQuery to
+            // parse that text into a document fragment using jQuery.clean().        
+            var templateTextInWrapper = "<script type=\"text/html\">" + getTemplateNode(template).text + "</script>";
+            var renderedMarkupInWrapper = $.tmpl(templateTextInWrapper, data);
+            var renderedMarkup = renderedMarkupInWrapper[0].text.replace(aposRegex, "'");;
+            return jQuery.clean([renderedMarkup], document);
+        }
+        
+        // It's easier with jquery.tmpl v2 and later - it handles any DOM structure
+        data = [data]; // Prewrap the data in an array to stop jquery-tmpl from trying to unwrap any arrays
+        var templateText = getTemplateNode(template).text;
+        return $.tmpl(templateText, data);
     },
 
     this.isTemplateRewritten = function (template) {
@@ -27,32 +49,45 @@ ko.jqueryTmplTemplateEngine = function () {
 
     this.rewriteTemplate = function (template, rewriterCallback) {
         var templateNode = getTemplateNode(template);
-        var rewritten = rewriterCallback(templateNode.text);
+        var rewritten = rewriterCallback(templateNode.text);     
         
-        // jquery.tmpl falls over if you use single-quotes, so replace these with a temporary marker for template rendering, 
-        // and then replace back after the template was rendered. This is slightly complicated by the fact that we must not interfere
-        // with any code blocks - only replace apos characters outside code blocks.
-        rewritten = ko.utils.stringTrim(rewritten);
-        rewritten = rewritten.replace(/([\s\S]*?)(\${[\s\S]*?}|{{[\=a-z][\s\S]*?}}|$)/g, function(match) {
-        	// Called for each non-code-block followed by a code block (or end of template)
-        	var nonCodeSnippet = arguments[1];
-        	var codeSnippet = arguments[2];
-        	return nonCodeSnippet.replace(/\'/g, aposMarker) + codeSnippet;
-        });        
+        if (this.jQueryTmplVersion == 1) {
+            // jquery.tmpl v1 falls over if you use single-quotes, so replace these with a temporary marker for template rendering, 
+            // and then replace back after the template was rendered. This is slightly complicated by the fact that we must not interfere
+            // with any code blocks - only replace apos characters outside code blocks.
+            rewritten = ko.utils.stringTrim(rewritten);
+            rewritten = rewritten.replace(/([\s\S]*?)(\${[\s\S]*?}|{{[\=a-z][\s\S]*?}}|$)/g, function(match) {
+                // Called for each non-code-block followed by a code block (or end of template)
+                var nonCodeSnippet = arguments[1];
+                var codeSnippet = arguments[2];
+                return nonCodeSnippet.replace(/\'/g, aposMarker) + codeSnippet;
+            });         	
+        }
         
         templateNode.text = rewritten;
         templateNode.isRewritten = true;
     },
 
     this.createJavaScriptEvaluatorBlock = function (script) {
-        return "{{= " + script + "}}";
+        if (this.jQueryTmplVersion == 1)
+            return "{{= " + script + "}}";
+            
+        // From v2, jquery-tmpl does some parameter parsing that fails on nontrivial expressions.
+        // Prevent it from messing with the code by wrapping it in a further function.
+        return "{{ko_code ((function() { return " + script + " })()) }}";
     },
 
-    // Am considering making template registration a native part of the API (and storing added templates centrally), but for now it's specific to this template engine
     this.addTemplate = function (templateName, templateMarkup) {
         document.write("<script type='text/html' id='" + templateName + "'>" + templateMarkup + "</script>");
     }
+    
+    if (this.jQueryTmplVersion > 1) {
+        jQuery.tmpl.tag.ko_code = {
+            open: "_.push($1 || '');"
+        };
+    }    
 };
+
 ko.jqueryTmplTemplateEngine.prototype = new ko.templateEngine();
 
 // Use this one by default
