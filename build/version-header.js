cf9ad3eda77f9dfed9a401d98f540c353d0afbf1
Steve Sanderson <steve@codeville.net>
2011-04-07 10:21:36 +0100

    Bumped version to 1.2.0rc and rebuilt

--- build/version-header.js
+++ build/version-header.js
@@ -1,4 +1,4 @@
-// Knockout JavaScript library v1.2.0pre
+// Knockout JavaScript library v1.2.0rc
 // (c) Steven Sanderson - http://knockoutjs.com/
 // License: MIT (http://www.opensource.org/licenses/mit-license.php)
 
