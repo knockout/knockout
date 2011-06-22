0ac38685af2d4dd488bc678ab25c7158462ef70f
antis <mono.xf@gmail.com>
2011-06-21 17:53:14 -0700

    Added prop binding

--- src/binding/defaultBindings.js
+++ src/binding/defaultBindings.js
@@ -414,3 +414,16 @@ ko.bindingHandlers['attr'] = {
         }
     }
 };
+
+ko.bindingHandlers['prop'] = {
+    update: function(element, valueAccessor, allBindingsAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor()) || {};
+        for (var propName in value) {
+            if (typeof propName == "string") {
+                var propValue = ko.utils.unwrapObservable(value[propName]);
+
+                element[propName] = propValue;
+            }
+        }
+    }
+}; 
