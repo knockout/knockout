0cbcbad2467f8b7ebffae0fb3d2e1bca2c630238
Steve Sanderson <steve@codeville.net>
2011-09-13 19:55:49 +0100

    Complete the preceding commit

--- build/output/knockout-latest.debug.js
+++ build/output/knockout-latest.debug.js
@@ -1440,20 +1440,21 @@ ko.exportSymbol('ko.jsonExpressionRewriting.insertPropertyAccessorsIntoJson', ko
         // e.g., from <div>OK</div><!-- ko blah --><span>Another</span>, returns: <!-- ko blah --><span>Another</span>
         //       from <div>OK</div><!-- /ko --><!-- /ko -->,             returns: <!-- /ko --><!-- /ko -->
         var childNode = node.firstChild, captureRemaining = null;
-        do {
-            if (captureRemaining)                   // We already hit an unbalanced node and are now just scooping up all subsequent nodes
-                captureRemaining.push(childNode);
-            else if (isStartComment(childNode)) {
-                var matchingEndComment = getMatchingEndComment(childNode, /* allowUnbalanced: */ true);
-                if (matchingEndComment)             // It's a balanced tag, so skip immediately to the end of this virtual set
-                    childNode = matchingEndComment;
-                else
-                    captureRemaining = [childNode]; // It's unbalanced, so start capturing from this point
-            } else if (isEndComment(childNode)) {
-                captureRemaining = [childNode];     // It's unbalanced (if it wasn't, we'd have skipped over it already), so start capturing
-            }
-        } while (childNode = childNode.nextSibling);
-
+        if (childNode) {
+            do {
+                if (captureRemaining)                   // We already hit an unbalanced node and are now just scooping up all subsequent nodes
+                    captureRemaining.push(childNode);
+                else if (isStartComment(childNode)) {
+                    var matchingEndComment = getMatchingEndComment(childNode, /* allowUnbalanced: */ true);
+                    if (matchingEndComment)             // It's a balanced tag, so skip immediately to the end of this virtual set
+                        childNode = matchingEndComment;
+                    else
+                        captureRemaining = [childNode]; // It's unbalanced, so start capturing from this point
+                } else if (isEndComment(childNode)) {
+                    captureRemaining = [childNode];     // It's unbalanced (if it wasn't, we'd have skipped over it already), so start capturing
+                }
+            } while (childNode = childNode.nextSibling);
+        }
         return captureRemaining;
     }
 
@@ -1545,21 +1546,23 @@ ko.exportSymbol('ko.jsonExpressionRewriting.insertPropertyAccessorsIntoJson', ko
             // Scan immediate children to see if they contain unbalanced comment tags. If they do, those comment tags
             // must be intended to appear *after* that child, so move them there.
             var childNode = elementVerified.firstChild;
-            do {
-                if (childNode.nodeType === 1) {
-                    var unbalancedTags = getUnbalancedChildTags(childNode);
-                    if (unbalancedTags) {
-                        // Fix up the DOM by moving the unbalanced tags to where they most likely were intended to be placed - *after* the child
-                        var nodeToInsertBefore = childNode.nextSibling;
-                        for (var i = 0; i < unbalancedTags.length; i++) {
-                            if (nodeToInsertBefore)
-                                elementVerified.insertBefore(unbalancedTags[i], nodeToInsertBefore);
-                            else
-                                elementVerified.appendChild(unbalancedTags[i]);
+            if (childNode) {
+                do {
+                    if (childNode.nodeType === 1) {
+                        var unbalancedTags = getUnbalancedChildTags(childNode);
+                        if (unbalancedTags) {
+                            // Fix up the DOM by moving the unbalanced tags to where they most likely were intended to be placed - *after* the child
+                            var nodeToInsertBefore = childNode.nextSibling;
+                            for (var i = 0; i < unbalancedTags.length; i++) {
+                                if (nodeToInsertBefore)
+                                    elementVerified.insertBefore(unbalancedTags[i], nodeToInsertBefore);
+                                else
+                                    elementVerified.appendChild(unbalancedTags[i]);
+                            }
                         }
                     }
-                }
-            } while (childNode = childNode.nextSibling)
+                } while (childNode = childNode.nextSibling);
+            }
         }  
     };  
 })();
