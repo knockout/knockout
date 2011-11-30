ab915e063bf095cd3be4309959d3e0ba11d24bf5
Matthew Smith <matthew.smith2@extendhealth.com>
2011-11-30 10:39:59 -0700

    Fixed bug in npm loading.

--- src/google-closure-compiler-utils.js
+++ src/google-closure-compiler-utils.js
@@ -1,10 +1,12 @@
 // Google Closure Compiler helpers (used only to make the minified file smaller)
 ko.exportSymbol = function(publicPath, object) {
-  var tokens = publicPath.split(".");
-  var target = globalRoot;
-  for (var i = 0; i < tokens.length - 1; i++)
-    target = target[tokens[i]];
-  target[tokens[tokens.length - 1]] = object;
+  if (typeof exports === 'undefined') {
+    var tokens = publicPath.split(".");
+    var target = window;
+    for (var i = 0; i < tokens.length - 1; i++)
+      target = target[tokens[i]];
+    target[tokens[tokens.length - 1]] = object;
+  }
 };
 ko.exportProperty = function(owner, publicName, object) {
   owner[publicName] = object;
