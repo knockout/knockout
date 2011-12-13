fca15fd3ef1acdb1f2c62e0388b24836618a283c
Steve Sanderson <steve@codeville.net>
2011-12-13 23:08:16 +0000

    Add spec to ensure browsers consistently pass args to event handlers where you use ".bind(...)"

--- spec/defaultBindingsBehaviors.js
+++ spec/defaultBindingsBehaviors.js
@@ -562,6 +562,31 @@ describe('Binding: Event', {
         ko.utils.triggerEvent(testNode.childNodes[0].childNodes[0], "click");
         value_of(model.innerWasCalled).should_be(true);    	
         value_of(model.outerWasCalled).should_be(false);    	
+    },
+
+    'Should be able to supply handler params using "bind" helper': function() {
+        // Using "bind" like this just eliminates the function literal wrapper - it's purely stylistic
+        var didCallHandler = false, someObj = {};
+        var myHandler = function() {
+            value_of(this).should_be(someObj);
+            value_of(arguments.length).should_be(5);
+
+            // First x args will be the ones you bound
+            value_of(arguments[0]).should_be(123);
+            value_of(arguments[1]).should_be("another");
+            value_of(arguments[2].something).should_be(true);
+
+            // Then you get the args we normally pass to handlers, i.e., the model then the event
+            value_of(arguments[3]).should_be(viewModel);
+            value_of(arguments[4].type).should_be("mouseover");
+
+            didCallHandler = true;
+        };
+        testNode.innerHTML = "<button data-bind='event:{ mouseover: myHandler.bind(someObj, 123, \"another\", { something: true }) }'>hey</button>";
+        var viewModel = { myHandler: myHandler, someObj: someObj };
+        ko.applyBindings(viewModel, testNode);
+        ko.utils.triggerEvent(testNode.childNodes[0], "mouseover");
+        value_of(didCallHandler).should_be(true);        
     }
 });
 
