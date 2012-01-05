6cd2156c2f20d4efb514344ed568a0a119170503
Steve Sanderson <steve@codeville.net>
2012-01-05 23:26:30 +0000

    Move some parts of the build system into a "/build/fragments" subfolder for tidiness

--- build/build-windows.bat
+++ build/build-windows.bat
@@ -2,7 +2,7 @@
 set OutDebugFile=output\knockout-latest.debug.js
 set OutMinFile=output\knockout-latest.js
 set AllFiles=
-for /f "eol=] skip=1 delims=' " %%i in (source-references.js) do set Filename=%%i& call :Concatenate 
+for /f "eol=] skip=1 delims=' " %%i in (fragments\source-references.js) do set Filename=%%i& call :Concatenate 
 
 goto :Combine
 :Concatenate 
@@ -14,21 +14,21 @@ goto :Combine
 goto :EOF 
 
 :Combine
-type amd-pre.js                   > %OutDebugFile%.temp
+type fragments\amd-pre.js         > %OutDebugFile%.temp
 type %AllFiles%                   >> %OutDebugFile%.temp
-type amd-post.js                  >> %OutDebugFile%.temp
+type fragments\amd-post.js        >> %OutDebugFile%.temp
 
 @rem Now call Google Closure Compiler to produce a minified version
 tools\curl -d output_info=compiled_code -d output_format=text -d compilation_level=ADVANCED_OPTIMIZATIONS --data-urlencode js_code@%OutDebugFile%.temp "http://closure-compiler.appspot.com/compile" > %OutMinFile%.temp
 
 @rem Finalise each file by prefixing with version header and surrounding in function closure
-copy /y version-header.js %OutDebugFile%
+copy /y fragments\version-header.js %OutDebugFile%
 echo (function(window,document,navigator,undefined){ >> %OutDebugFile%
 type %OutDebugFile%.temp                             >> %OutDebugFile%
 echo })(window,document,navigator);                  >> %OutDebugFile%
 del %OutDebugFile%.temp
 
-copy /y version-header.js %OutMinFile%
+copy /y fragments\version-header.js %OutMinFile%
 echo (function(window,document,navigator,undefined){ >> %OutMinFile%
 type %OutMinFile%.temp                               >> %OutMinFile%
 echo })(window,document,navigator);                  >> %OutMinFile%
