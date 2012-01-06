65fee8d66f68ef39f0195497a47c4a99486133c9
Steve Sanderson <steve@codeville.net>
2012-01-06 00:52:37 +0000

    Temporarily support only AMD (not exports/module.exports) until we clarify how to avoid a breaking change

--- build/fragments/amd-pre.js
+++ build/fragments/amd-pre.js
@@ -1,15 +1,10 @@
 !function(factory) {
-    // Support three module loading scenarios
-    if (typeof exports !== 'undefined') {
-        // [1] CommonJS/Node.js
-        if (typeof module !== 'undefined' && module['exports'])
-            exports = module['exports']; // For Node.js
-        factory(exports);
-    } else if (typeof define === 'function' && define['amd'] && define['amd']['ko']) {
-        // [2] AMD anonymous module
+    // Support AMD where available and opted in via define.amd.ko
+    if (typeof define === 'function' && define['amd'] && define['amd']['ko']) {
+        // Register as an AMD anonymous module
         define(['exports'], factory);
     } else {
-        // [3] No module loader (plain <script> tag) - put directly in global namespace
+        // AMD not in use (e.g., when referenced via aplain <script> tag) - put ko directly in global namespace
         factory(window['ko'] = {});
     }
 }(function(koExports){
