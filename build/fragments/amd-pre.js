cf426bee91b2ac557cf5bdf5b66422bba97c4e51
Steve Sanderson <steve@codeville.net>
2012-01-06 00:36:21 +0000

    Update amd-pre.js to use AMD *only* when define.amd.ko is true, to avoid breaking backward compatibility. (Also tweak indentation style to match conventions)

--- build/fragments/amd-pre.js
+++ build/fragments/amd-pre.js
@@ -1,16 +1,15 @@
-!function(factory){
-  // Export the ko object for NodeJs and CommonJs with 
-  // backwards compatability for the old `require()` API.
-  // If we're not in CommonJs, add `ko` to the global object.
-  if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports){
-      exports = module.exports;
+!function(factory) {
+    // Support three module loading scenarios
+    if (typeof exports !== 'undefined') {
+        // [1] CommonJS/Node.js
+        if (typeof module !== 'undefined' && module['exports'])
+            exports = module['exports']; // For Node.js
+        factory(exports);
+    } else if (typeof define === 'function' && define['amd'] && define['amd']['ko']) {
+        // [2] AMD anonymous module
+        define(['exports'], factory);
+    } else {
+        // [3] No module loader (plain <script> tag) - put directly in global namespace
+        factory(window['ko'] = {});
     }
-    factory(exports);
-  } else if (typeof define === 'function' && define.amd) {
-    // Register as a named module with AMD
-    define(['exports'], factory);
-  } else {
-    factory(window['ko'] = {});
-  }
 }(function(koExports){
