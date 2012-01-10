c9abfe6008a4f1e6c2b2e971e96517fdd3ab5786
Steve Sanderson <steve@codeville.net>
2012-01-10 22:57:20 +0000

    Update build script to include ko.version (selectively merged from mtscout6's "versioning" branch - thanks Matt!)

--- build/build-windows.bat
+++ build/build-windows.bat
@@ -4,8 +4,18 @@ set OutMinFile=output\knockout-latest.js
 set AllFiles=
 for /f "eol=] skip=1 delims=' " %%i in (fragments\source-references.js) do set Filename=%%i& call :Concatenate 
 
+set /p Version= <fragments\version.txt
+set VersionHeaderPath=fragments\version-header.js
+set VersionJsFile=..\src\version.js
+tools\sed "s/##VERSION##/%Version%/" <%VersionHeaderPath% > %VersionHeaderPath%.temp
+tools\sed "s/Working-Debug/%Version%/" <%VersionJsFile% > %VersionJsFile%.temp
+
 goto :Combine
 :Concatenate 
+    if /i "%Filename%"=="src/version.js" (
+        set Filename="src/version.js.temp"
+    )
+
     if /i "%AllFiles%"=="" ( 
         set AllFiles=..\%Filename:/=\%
     ) else ( 
@@ -22,14 +32,17 @@ type fragments\amd-post.js        >> %OutDebugFile%.temp
 tools\curl -d output_info=compiled_code -d output_format=text -d compilation_level=ADVANCED_OPTIMIZATIONS --data-urlencode js_code@%OutDebugFile%.temp "http://closure-compiler.appspot.com/compile" > %OutMinFile%.temp
 
 @rem Finalise each file by prefixing with version header and surrounding in function closure
-copy /y fragments\version-header.js %OutDebugFile%
+copy /y fragments\version-header.js.temp %OutDebugFile%
 echo (function(window,document,navigator,undefined){ >> %OutDebugFile%
 type %OutDebugFile%.temp                             >> %OutDebugFile%
 echo })(window,document,navigator);                  >> %OutDebugFile%
 del %OutDebugFile%.temp
 
-copy /y fragments\version-header.js %OutMinFile%
+copy /y fragments\version-header.js.temp %OutMinFile%
 echo (function(window,document,navigator,undefined){ >> %OutMinFile%
 type %OutMinFile%.temp                               >> %OutMinFile%
 echo })(window,document,navigator);                  >> %OutMinFile%
 del %OutMinFile%.temp
+
+del %VersionHeaderPath%.temp
+del %VersionJsFile%.temp
--- build/fragments/version-header.js
+++ build/fragments/version-header.js
@@ -1,4 +1,4 @@
-// Knockout JavaScript library v2.1.0pre
+// Knockout JavaScript library v##VERSION##
 // (c) Steven Sanderson - http://knockoutjs.com/
 // License: MIT (http://www.opensource.org/licenses/mit-license.php)
 
