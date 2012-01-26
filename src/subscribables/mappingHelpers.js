416ef0d7f6b06e54fec525b510ca366d2c1cfa0b
Steve Sanderson <steve@codeville.net>
2012-01-26 00:14:00 +0000

    Respect custom toJSON functions on ko.toJSON-mapped arrays (fixes #278)

--- src/subscribables/mappingHelpers.js
+++ src/subscribables/mappingHelpers.js
@@ -58,6 +58,10 @@
         if (rootObject instanceof Array) {
             for (var i = 0; i < rootObject.length; i++)
                 visitorCallback(i);
+            
+            // For arrays, also respect toJSON property for custom mappings (fixes #278)
+            if (typeof rootObject['toJSON'] == 'function')
+                visitorCallback('toJSON');
         } else {
             for (var propertyName in rootObject)
                 visitorCallback(propertyName);
