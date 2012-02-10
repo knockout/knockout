82e9ecc86b7c7ee84600787602735fb58ba6251a
Trey Shugart <treshugart@gmail.com>
2012-02-10 11:23:20 +1100

    Fix issue where 'class' attribute in the 'attr' binding does not workin IE7 or lower. In those instances, one would have to use 'className' and 'class' to support all browsers.

--- src/binding/defaultBindings.js
+++ src/binding/defaultBindings.js
@@ -432,6 +432,11 @@ ko.bindingHandlers['attr'] = {
             if (typeof attrName == "string") {
                 var attrValue = ko.utils.unwrapObservable(value[attrName]);
                 
+				// to fix issues in IE, if the attribute name is "class", we make it "className"
+				if (attrName === 'class' && /(MSIE\s6|MSIE\s7)/.test(navigator.userAgent)) {
+					attrName = 'className';
+				}
+				
                 // To cover cases like "attr: { checked:someProp }", we want to remove the attribute entirely 
                 // when someProp is a "no value"-like value (strictly null, false, or undefined)
                 // (because the absence of the "checked" attr is how to mark an element as not checked, etc.)                
