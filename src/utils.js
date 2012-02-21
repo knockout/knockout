42e06bf09355c5545cc313904db42b6b02f7b516
GilesBradshaw <giles.bradshaw@sigyl.com>
2012-02-21 13:39:32 +0000

    Fix css binding so it sets className for svg nodes

--- src/utils.js
+++ src/utils.js
@@ -269,21 +269,29 @@ ko.utils = new (function () {
         },
 
         domNodeHasCssClass: function (node, className) {
-            var currentClassNames = (node.className || "").split(/\s+/);
+            var currentClassNames = typeof (node.className) == "string" ? (node.className || "").split(/\s+/) : (node.className.baseVal || "").split(/\s+/);
             return ko.utils.arrayIndexOf(currentClassNames, className) >= 0;
         },
 
         toggleDomNodeCssClass: function (node, className, shouldHaveClass) {
             var hasClass = ko.utils.domNodeHasCssClass(node, className);
             if (shouldHaveClass && !hasClass) {
-                node.className = (node.className || "") + " " + className;
+                if (typeof (node.className) == "string")
+                    node.className = (node.className || "") + " " + className;
+                else {
+                    if ("baseVal" in node.className) node.className.baseVal = (node.className.baseVal || "") + " " + className;
+                }
             } else if (hasClass && !shouldHaveClass) {
-                var currentClassNames = (node.className || "").split(/\s+/);
+                var currentClassNames = typeof (node.className) == "string" ? (node.className || "").split(/\s+/) : (node.className.baseVal || "").split(/\s+/);
                 var newClassName = "";
                 for (var i = 0; i < currentClassNames.length; i++)
                     if (currentClassNames[i] != className)
                         newClassName += currentClassNames[i] + " ";
-                node.className = ko.utils.stringTrim(newClassName);
+                if (typeof (node.className) == "string") {
+                    node.className = ko.utils.stringTrim(newClassName);
+                } else {
+                    if ("baseVal" in node.className) node.className.baseVal = ko.utils.stringTrim(newClassName);
+                }
             }
         },
 
