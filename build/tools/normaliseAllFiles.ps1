8d7481a126a2f92a595f3669eb0d94d78820b583
Steve Sanderson <steve@codeville.net>
2012-04-13 10:51:57 +0100

    Normalisation script now trims trailing whitespace from lines

--- build/tools/normaliseAllFiles.ps1
+++ build/tools/normaliseAllFiles.ps1
@@ -3,9 +3,9 @@
 $allFiles = (Get-ChildItem -Path ..\..\ -Include *.js,*.css,*.bat -Recurse)
 
 $allFiles | %{
-    # Read file, output back to file in CP1252 format (default for Git on Windows)
+    # Read file, remove trailing spaces/tabs, output back to file in CP1252 format (default for Git on Windows)
     # This will also ensure the file has a trailing linebreak
     Write-Host "Processing $_..."
-    $fileContent = Get-Content $_    
+    $fileContent = (Get-Content $_) -replace "[ \t]+$", ""
     [System.IO.File]::WriteAllLines($_, $fileContent, [System.Text.Encoding]::GetEncoding(1252))
 }
