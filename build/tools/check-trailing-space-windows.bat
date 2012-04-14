82e541b8c39799b8c6b2ea41159998d933a1806c
Steve Sanderson <steve@codeville.net>
2012-04-14 09:37:16 +0100

    Amend build checks to disallow trailing whitespace on blank-only lines (so there is now one consistent standard both for normalisation and for build)

--- build/tools/check-trailing-space-windows.bat
+++ build/tools/check-trailing-space-windows.bat
@@ -3,7 +3,7 @@
 @rem Check that all files have trailing spaces stripped
 set OutTrailingSpaceListFile=output\knockout-files-to-clean.txt
 cd ..
-findstr -nrs -c:"[^ ][ ][ ]*$" *.js *.html *.css *.bat *.ps1 |findstr -rv "^build\\output" > build\%OutTrailingSpaceListFile%
+findstr -nrs -c:"[ ]$" *.js *.html *.css *.bat *.ps1 | findstr -rv "^build\\output" > build\%OutTrailingSpaceListFile%
 cd build
 for %%R in (%OutTrailingSpaceListFile%) do if %%~zR gtr 0 goto :NeedFixTrailingSpace
 del %OutTrailingSpaceListFile%
