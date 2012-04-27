d436d5ce9eb49a04a51cc8838b5cf835e1c0e931
Christian Dalager <christian@dalager.com>
2012-04-27 12:59:42 +0200

    Fixing bug where ie9 compatibility mode breaks on document.createElement
    http://msdn.microsoft.com/en-us/library/ie/ff986077(v=vs.85).aspx

--- src/binding/defaultBindings.js
+++ src/binding/defaultBindings.js
@@ -363,8 +363,11 @@ ko.bindingHandlers['uniqueName'] = {
             // Workaround IE 6/7 issue
             // - https://github.com/SteveSanderson/knockout/issues/197
             // - http://www.matts411.com/post/setting_the_name_attribute_in_ie_dom/
-            if (ko.utils.isIe6 || ko.utils.isIe7)
-                element.mergeAttributes(document.createElement("<input name='" + element.name + "'/>"), false);
+            if (ko.utils.isIe6 || ko.utils.isIe7){
+                var newEl = document.createElement("input");
+                newEl.setAttribute("name", element.name);
+                element.mergeAttributes(newEl, false);
+            }
         }
     }
 };
