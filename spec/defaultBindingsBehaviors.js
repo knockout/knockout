fb0e4c43e370f3d37855d2d9a641f099a98ec893
Michael Best <mbest@dasya.com>
2012-05-14 17:21:29 -1000

    #460 - Always use value attribute, if specified, even if blank.
    Add spec to show that a blank value will be used instead of the option text. Modern browsers automatically set element.value to the text if the value attribute is missing. IE<=7 doesn't and also makes it kind of hard to tell of the value attribute is set or not.

--- spec/defaultBindingsBehaviors.js
+++ spec/defaultBindingsBehaviors.js
@@ -379,6 +379,18 @@ describe('Binding: Value', {
         value_of(observable()).should_be(20);
     },
 
+    'For select boxes with values attributes, should always use value (and not text)': function() {
+        var observable = new ko.observable('A');
+        testNode.innerHTML = "<select data-bind='value:myObservable'><option value=''>A</option><option value='A'>B</option></select>";
+        ko.applyBindings({ myObservable: observable }, testNode);
+        var dropdown = testNode.childNodes[0];
+        value_of(dropdown.selectedIndex).should_be(1);
+
+        dropdown.selectedIndex = 0;
+        ko.utils.triggerEvent(dropdown, "change");
+        value_of(observable()).should_be("");
+    },
+
     'For select boxes with text values but no value property, should use text value': function() {
         var observable = new ko.observable('B');
         testNode.innerHTML = "<select data-bind='value:myObservable'><option>A</option><option>B</option><option>C</option></select>";
