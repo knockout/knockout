0b21a261d1006c5fcd60198c17437287df96adce
Michael Best <mbest@dasya.com>
2012-07-03 11:32:12 -1000

    Fix for #452: setElementName error IE9 in compatibility mode

--- src/utils.js
+++ src/utils.js
@@ -321,8 +321,12 @@ ko.utils = new (function () {
             // Workaround IE 6/7 issue
             // - https://github.com/SteveSanderson/knockout/issues/197
             // - http://www.matts411.com/post/setting_the_name_attribute_in_ie_dom/
-            if (ieVersion <= 7)
-                element.mergeAttributes(document.createElement("<input name='" + element.name + "'/>"), false);
+            if (ieVersion <= 7) {
+                try {
+                    element.mergeAttributes(document.createElement("<input name='" + element.name + "'/>"), false);
+                }
+                catch(e) {}
+            }
         },
 
         forceRefresh: function(node) {
