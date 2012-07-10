cfbc63aae8868e8ad7a6bdfa1ac522086ddc2fdf
Steve Sanderson <steve@codeville.net>
2012-07-10 10:53:28 +0100

    Disable dependency detection during subscriber callbacks to fix #341

--- src/subscribables/dependencyDetection.js
+++ src/subscribables/dependencyDetection.js
@@ -16,10 +16,19 @@ ko.dependencyDetection = (function () {
                 throw new Error("Only subscribable things can act as dependencies");
             if (_frames.length > 0) {
                 var topFrame = _frames[_frames.length - 1];
-                if (ko.utils.arrayIndexOf(topFrame.distinctDependencies, subscribable) >= 0)
+                if (!topFrame || ko.utils.arrayIndexOf(topFrame.distinctDependencies, subscribable) >= 0)
                     return;
                 topFrame.distinctDependencies.push(subscribable);
                 topFrame.callback(subscribable);
+            }
+        },
+
+        ignore: function(callback, callbackTarget) {
+            try {
+                _frames.push(null);
+                callback.call(callbackTarget);
+            } finally {
+                _frames.pop();
             }
         }
     };
