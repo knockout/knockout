afdbfc23f988e78237dfd03ade05fe5fbad18800
Michael Best <mbest@dasya.com>
2012-07-25 13:57:30 -1000

    #352 - Prevent hasfocus causing a loss of focus in all browsers when changing tabs/windows by setting a flag that prevents it from calling 'blur()' on the element when it loses focus.

--- src/binding/defaultBindings.js
+++ src/binding/defaultBindings.js
@@ -457,29 +457,39 @@ ko.bindingHandlers['attr'] = {
     }
 };
 
+var hasfocusUpdatingProperty = '__ko_hasfocusUpdating';
 ko.bindingHandlers['hasfocus'] = {
     'init': function(element, valueAccessor, allBindingsAccessor) {
         var handleElementFocusChange = function(isFocused) {
             // Where possible, ignore which event was raised and determine focus state using activeElement,
             // as this avoids phantom focus/blur events raised when changing tabs in modern browsers.
-            // However, not all KO-targeted browsers (Firefox 2) support activeElement.
+            // However, not all KO-targeted browsers (Firefox 2) support activeElement. For those browsers,
+            // prevent a loss of focus when changing tabs/windows by setting a flag that prevents hasfocus
+            // from calling 'blur()' on the element when it loses focus.
             // Discussion at https://github.com/SteveSanderson/knockout/pull/352
+            element[hasfocusUpdatingProperty] = true;
             var ownerDoc = element.ownerDocument;
             if ("activeElement" in ownerDoc) {
                 isFocused = (ownerDoc.activeElement === element);
             }
             var modelValue = valueAccessor();
             ko.jsonExpressionRewriting.writeValueToProperty(modelValue, allBindingsAccessor, 'hasfocus', isFocused, true);
+            element[hasfocusUpdatingProperty] = false;
         };
-        ko.utils.registerEventHandler(element, "focus", function() { handleElementFocusChange(true) });
-        ko.utils.registerEventHandler(element, "focusin", function() { handleElementFocusChange(true) }); // For IE
-        ko.utils.registerEventHandler(element, "blur",  function() { handleElementFocusChange(false) });
-        ko.utils.registerEventHandler(element, "focusout",  function() { handleElementFocusChange(false) }); // For IE
+        var handleElementFocusIn = handleElementFocusChange.bind(null, true);
+        var handleElementFocusOut = handleElementFocusChange.bind(null, false);
+
+        ko.utils.registerEventHandler(element, "focus", handleElementFocusIn);
+        ko.utils.registerEventHandler(element, "focusin", handleElementFocusIn); // For IE
+        ko.utils.registerEventHandler(element, "blur",  handleElementFocusOut);
+        ko.utils.registerEventHandler(element, "focusout",  handleElementFocusOut); // For IE
     },
     'update': function(element, valueAccessor) {
-        var value = ko.utils.unwrapObservable(valueAccessor());
-        value ? element.focus() : element.blur();
-        ko.utils.triggerEvent(element, value ? "focusin" : "focusout"); // For IE, which doesn't reliably fire "focus" or "blur" events synchronously
+        if (!element[hasfocusUpdatingProperty]) {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            value ? element.focus() : element.blur();
+            ko.utils.triggerEvent(element, value ? "focusin" : "focusout"); // For IE, which doesn't reliably fire "focus" or "blur" events synchronously
+        }
     }
 };
 
