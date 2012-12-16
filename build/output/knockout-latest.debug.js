d47350e3fd5f0baabe4be3c25e9352ba266304c3
Brian Wigfield <brian.wigfield@adventuretechgroup.com>
2012-12-16 17:42:24 -0600

    Added string interpolation to text binding

--- build/output/knockout-latest.debug.js
+++ build/output/knockout-latest.debug.js
@@ -2619,13 +2619,41 @@ ko.bindingHandlers['submit'] = {
         });
     }
 };
+var startingTextDomDataKey = '__ko_startingText';
 ko.bindingHandlers['text'] = {
-    'update': function (element, valueAccessor) {
-        ko.utils.setTextContent(element, valueAccessor());
-    }
+	init: function(element, valueAccessor) {
+		//cache any starting text
+		var firstChild = ko.virtualElements.firstChild(element);
+		if (!firstChild)
+			return;
+		var startingText = ko.virtualElements.firstChild(element).data;
+		if(startingText)
+			element[startingTextDomDataKey] = startingText;
+	},
+	update: function(element, valueAccessor) {
+		var value = ko.utils.unwrapObservable(valueAccessor());
+		//if the binding is not an array, replace the contents
+		if(toString.call(value) !== '[object Array]')
+			return ko.utils.setTextContent(element, value);
+
+		//if it is an array then apply interpolation
+		var args = [];
+		for(var arg in value) {
+			args.push(ko.utils.unwrapObservable(value[arg]));
+		}
+	
+		var formattedText = element[startingTextDomDataKey].replace(/{(\d+)}/g, function(match, number) {
+			if (args[number] === void 0) {
+				return match;
+			} else {
+				return args[number];
+			}
+		});
+		ko.utils.setTextContent(element, formattedText);
+	}
 };
-ko.virtualElements.allowedBindings['text'] = true;
-ko.bindingHandlers['uniqueName'] = {
+
+ko.virtualElements.allowedBindings['text'] = true;ko.bindingHandlers['uniqueName'] = {
     'init': function (element, valueAccessor) {
         if (valueAccessor()) {
             var name = "ko_unique_" + (++ko.bindingHandlers['uniqueName'].currentIndex);
--- src/binding/defaultBindings/text.js
+++ src/binding/defaultBindings/text.js
@@ -1,6 +1,35 @@
+var startingTextDomDataKey = '__ko_startingText';
 ko.bindingHandlers['text'] = {
-    'update': function (element, valueAccessor) {
-        ko.utils.setTextContent(element, valueAccessor());
-    }
+	init: function(element, valueAccessor) {
+		//cache any starting text
+		var firstChild = ko.virtualElements.firstChild(element);
+		if (!firstChild)
+			return;
+		var startingText = ko.virtualElements.firstChild(element).data;
+		if(startingText)
+			element[startingTextDomDataKey] = startingText;
+	},
+	update: function(element, valueAccessor) {
+		var value = ko.utils.unwrapObservable(valueAccessor());
+		//if the binding is not an array, replace the contents
+		if(toString.call(value) !== '[object Array]')
+			return ko.utils.setTextContent(element, value);
+
+		//if it is an array then apply interpolation
+		var args = [];
+		for(var arg in value) {
+			args.push(ko.utils.unwrapObservable(value[arg]));
+		}
+	
+		var formattedText = element[startingTextDomDataKey].replace(/{(\d+)}/g, function(match, number) {
+			if (args[number] === void 0) {
+				return match;
+			} else {
+				return args[number];
+			}
+		});
+		ko.utils.setTextContent(element, formattedText);
+	}
 };
+
 ko.virtualElements.allowedBindings['text'] = true;
