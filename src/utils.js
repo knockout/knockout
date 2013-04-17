fcbf52f36ff5336a9f666ebb9404193f38c73aa9
Brian Di Palma <offler@gmail.com>
2013-04-17 21:01:59 +0100

    Changed Function.bind to improve performance when there are no arguments to be bound (when there is no partial application).

--- src/utils.js
+++ src/utils.js
@@ -550,9 +550,17 @@ if (!Function.prototype['bind']) {
     // Function.prototype.bind is a standard part of ECMAScript 5th Edition (December 2009, http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-262.pdf)
     // In case the browser doesn't implement it natively, provide a JavaScript implementation. This implementation is based on the one in prototype.js
     Function.prototype['bind'] = function (object) {
-        var originalFunction = this, args = Array.prototype.slice.call(arguments), object = args.shift();
-        return function () {
-            return originalFunction.apply(object, args.concat(Array.prototype.slice.call(arguments)));
-        };
+        var originalFunction = this;
+		
+		if (arguments.length === 1) {
+			return function () {
+				return originalFunction.apply(object);
+			};
+        } else {
+			var args = Array.prototype.slice.call(arguments, 1);
+			return function () {
+				return originalFunction.apply(object, args.concat(Array.prototype.slice.call(arguments)));
+			};
+        }
     };
 }
