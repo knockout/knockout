b894eaeabb4f40ca5c70b00be6818061d620aaab
scommisso <scommisso@godaddy.com>
2013-09-18 16:55:06 -0700

    Solving issue #816: https://github.com/knockout/knockout/issues/816.
    Default "throttle" algorithm is akin to jquery-throttle "debounce" algorithm.

--- src/subscribables/extenders.js
+++ src/subscribables/extenders.js
@@ -1,10 +1,39 @@
 ko.extenders = {
-    'throttle': function(target, timeout) {
+    'throttle': function(target, options) {
         // Throttling means two things:
 
         // (1) For dependent observables, we throttle *evaluations* so that, no matter how fast its dependencies
-        //     notify updates, the target doesn't re-evaluate (and hence doesn't notify) faster than a certain rate
-        target['throttleEvaluation'] = timeout;
+        //     notify updates, the target doesn't re-evaluate (and hence doesn't notify) faster than a certain rate.
+		//     By default, use "debounce" algorithm, which will limit evaluations until changes have stopped
+		//     for a certain period of time. Optionally, "throttle" will re-evaluate at a specified rate.
+		var throttleType = 'debounce',
+			throttleRate = 0,
+			throttleNoTrailingEval = false,
+			optionsValid;
+		if (typeof options === 'number') {
+			// For backwards compatibility, allow numeric parameter and default to "debounce".
+			throttleRate = options;
+		}
+		else {
+			throttleType = options.type;
+			throttleRate = options.delay;
+			throttleNoTrailingEval = options.noTrailing === true;
+		}
+		
+		optionsValid = (
+			throttleRate >= 0
+			&& (throttleType === 'debounce'
+				|| throttleType === 'throttle')
+			);
+		
+		if (!optionsValid) {
+			return target; // do not modify
+		}
+		else {
+			target['throttleType'] = throttleType;
+			target['throttleEvaluation'] = throttleRate;
+			target['throttleNoTrailing'] = throttleNoTrailingEval;
+		}
 
         // (2) For writable targets (observables, or writable dependent observables), we throttle *writes*
         //     so the target cannot change value synchronously or faster than a certain rate
@@ -12,10 +41,11 @@ ko.extenders = {
         return ko.dependentObservable({
             'read': target,
             'write': function(value) {
-                clearTimeout(writeTimeoutInstance);
-                writeTimeoutInstance = setTimeout(function() {
-                    target(value);
-                }, timeout);
+				// TODO: Apply "throttle" algorithm to writes, currently uses "debounce"
+				clearTimeout(writeTimeoutInstance);
+				writeTimeoutInstance = setTimeout(function() {
+					target(value);
+				}, throttleRate);
             }
         });
     },
