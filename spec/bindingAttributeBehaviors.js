b5ce6bd1efe72e04d11bcb73e5f32d73bebc18e5
Ryan Niemeyer <rniemeyer@gmail.com>
2013-09-28 22:46:58 -0500

    Fix "Should not bind against text content inside <script> tags" in IE<9

--- spec/bindingAttributeBehaviors.js
+++ spec/bindingAttributeBehaviors.js
@@ -437,7 +437,14 @@ describe('Binding attribute syntax', function() {
 
         var originalBindingProvider = ko.bindingProvider.instance;
         ko.bindingProvider.instance = {
-            nodeHasBindings: function(node, bindingContext) {
+            nodeHasBindings: function(node) {
+                // IE < 9 can't bind text nodes, as expando properties are not allowed on them
+                // this will still prove that the binding provider was not executed on the children of a script tag
+                if (node.nodeType === 3 && ko.utils.ieVersion < 9) {
+                    node.data = "replaced";
+                    return false;
+                }
+
                 return true;
             },
             getBindingAccessors: function(node, bindingContext) {
