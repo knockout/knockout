7950696668aa6b50ff0a6e756a0bc6dc232b34dd
Giovanni Lovato <heruan@aldu.net>
2013-10-30 11:32:40 +0100

    Normalize build filenames

--- spec/runner.node.js
+++ spec/runner.node.js
@@ -25,7 +25,7 @@ if (process.argv.length > 2 && process.argv[2] == '--source') {
     };
     require('../build/fragments/source-references');
 } else {
-    global.ko = require('../build/output/knockout-latest.js');
+    global.ko = require('../build/output/knockout.min.js');
 }
 
 // reference behaviors that should work out of browser
