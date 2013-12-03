35b27881c54867d3dd8a2adf95a3d63deadcbb75
Tim Tucker <tim@timtucker.com>
2013-12-03 11:25:40 -0500

    Change makeArray to just wrap arrayPushAll
    The code in makeArray and arrayPushAll does almost exactly the same thing, with the caveat that arrayPushAll has an optimization if the object being passed in is already an array.  This just puts everything through the same code path.
    
    Originally part of #1199, separated out since it's just simple refactoring and not really related to the other changes there.

--- src/utils.js
+++ src/utils.js
@@ -442,11 +442,7 @@ ko.utils = (function () {
         },
 
         makeArray: function(arrayLikeObject) {
-            var result = [];
-            for (var i = 0, j = arrayLikeObject.length; i < j; i++) {
-                result.push(arrayLikeObject[i]);
-            };
-            return result;
+            return ko.utils.arrayPushAll([], arrayLikeObject);
         },
 
         isIe6 : isIe6,
