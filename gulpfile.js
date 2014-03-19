2d02728f94b14d9351751f1ec6091f9a857c0427
Brian M Hunt <brianmhunt@gmail.com>
2014-03-19 09:50:49 -0400

    Include release and bump-{patch,minor,major} tasks
    Also create runner.{jquery,modernizr}.html that includes respective
    scripts.
    Note: the test runners are broken pending a workaround for
    gulp-inject(CDN); see https://github.com/klei/gulp-inject/issues/18

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,36 @@
 /*global module:false
 
-        Gulp for Knockout
-        ~~~~~~~~~~~~~~~~~
+        Gulp task runner for
+        Knockout
+        ~~~~~~~~~~~~~~~~~~~~
 
     $ gulp [task]
+
+Tasks
+-----
+
+$ gulp        (default task)
+: clean, build, build-debug
+
+$ gulp watch
+: runner
+Starts a livereload server that causes runner[.jquery,.modernizr].html
+to reload whenever the source or spec scripts are updated.
+
+$ gulp build
+Creates build/output/knockout.min.js
+
+$ gulp build-debug
+Creates build/output/knockout.debug.js
+
+$ gulp (bump-patch | bump-minor | bump-major)
+Increments the version in ./*.json by the corresponding level. e.g.
+gulp bump-patch on 3.1.0 will increment to 3.1.1.
+
+$ gulp release
+: clean, build, build-debug
+Tag and publish a version of the repository that includes dist/knockout.js
+and dist/knockout.debug.js
 
  */
 var
@@ -36,8 +63,18 @@ var
         scriptsDir + 'extern-post.js'
     ]),
 
+    CDN_ROOT = "http://cdnjs.cloudflare.com/ajax/libs/",
+    JASMINE_CDN = CDN_ROOT + "jasmine/1.3.1/jasmine.js",
+    JASMINE_HTML_CDN = CDN_ROOT + "jasmine/1.3.1/jasmine-html.js",
+    // jQuery & Modernizr are optional; see runner task
+    JQUERY_CDN = CDN_ROOT + "jquery/1.11.0/jquery.min.js",
+    MODERNIZR_CDN = CDN_ROOT + "modernizr/2.7.1/modernizr.min.js",
+
     // scripts that are loaded by the browser during testing
     runner_scripts = [].concat.apply([], [
+        "http://localhost:35728/livereload.js",
+        JASMINE_CDN,
+        JASMINE_HTML_CDN,
         "spec/helpers/beforeEach.js",
         "spec/helpers/jasmine.browser.js",
         // knockout polyfills
@@ -53,8 +90,10 @@ var
     buildDir = 'build/output/',
     build = {
         debug: 'knockout-latest.debug.js',
-        main: 'knockout.js',
+        main: 'knockout-latest.js',
     },
+
+    PACKAGE_FILES = ["./package.json", "./bower.json"],
 
     // Compiler options
     uglifyOptions = {
@@ -115,7 +154,6 @@ gulp.task('build-debug', function () {
 gulp.task("build", function () {
     gulp.src(scripts)
         .pipe(plugins.concat(build.main))
-        .pipe(plugins.rename({ extname: ".min.js" }))
         .pipe(plugins.uglify(uglifyOptions))
         .pipe(plugins.header(banner, { pkg: pkg }))
         .pipe(gulp.dest(buildDir))
@@ -130,17 +168,43 @@ gulp.task("watch", ['runner'], function () {
 })
 
 
+function bump(level) {
+    return function () {
+        gulp.src(PACKAGE_FILES)
+            .pipe(plugins.bump({type: level}))
+            .pipe(gulp.dest("./"))
+    }
+}
+
+gulp.task("bump-patch", bump('patch'))
+gulp.task("bump-minor", bump('minor'))
+gulp.task("bump-major", bump('major'))
+
+
+gulp.task("release", ['build', 'build-debug'], function (done) {
+    var version = "v" + pkg.version;
+    require('./release')(version);
+})
+
+
 gulp.task("runner", function () {
     // Build runner.html in the root directory. This makes it more
     // straightforward to access relative-path src/ and spec/ files i.e.
     // there will be no "../" in our <script> tags.
-    inject_options = {
-        addRootSlash: false
-    }
+    var inject_options = { addRootSlash: false },
+        SKIP = { read: false };
     gulp.src("spec/helpers/runner.template.html")
+        // create vanilla runner
         .pipe(plugins.rename("runner.html"))
-        .pipe(plugins.inject(gulp.src(runner_scripts, {read: false}),
-            inject_options))
+        .pipe(plugins.inject(runner_scripts), inject_options)
+        .pipe(gulp.dest("./"))
+        // create runner with jquery
+        .pipe(plugins.inject(JQUERY_CDN))
+        .pipe(plugins.rename("runner.jquery.html"))
+        .pipe(gulp.dest("./"))
+        // create runner with modernizr
+        .pipe(plugins.inject(MODERNIZR_CDN))
+        .pipe(plugins.rename("runner.modernizr.html"))
         .pipe(gulp.dest("./"))
 })
 
---
+++ release.js
@@ -0,0 +1,98 @@
+/*
+        Release
+        ~~~~~~~
+
+This will create a new release of the package.
+
+See https://github.com/knockout/knockout/issues/1039#issuecomment-37897655
+
+In particular it:
+
+A. Creates dest/ and copies build/output/knockout.js and build/output/knockout-latest.debug.js to dest/
+
+B.
+ 1. git add dist -f
+ 2. git commit -a -m "<MSG>"
+ 3. git tag "<VERSION>"
+ 4. git reset HEAD^1
+ 5. git push --tags
+
+C. removes dest/
+ */
+var fs = require('fs'),
+    gulp = require('gulp'),
+    git = require('gulp-git'),
+    gutil = require('gulp-util'),
+    exec = require('child_process').exec,
+
+    DEST = "./dest/",
+
+    min_copy = {
+      src: './build/output/knockout-latest.js',
+      dest: DEST + 'knockout.min.js'
+    },
+
+    debug_copy = {
+      src: './build/output/knockout-latest.debug.js',
+      dest: DEST + 'knockout.debug.js'
+    },
+
+    target_version;
+
+
+gulp.task("add-dist", function () {
+  gulp.src(DEST)
+      .pipe(git.add({args: "-f"}))
+})
+
+
+gulp.task("commit", ['add-dist'], function () {
+  gulp.src("./")
+      .pipe(git.commit("(task) Release " + target_version))
+})
+
+
+gulp.task("tag", ['commit'], function (done) {
+  var message = "(task) Tagging " + target_version,
+      cmd = 'git tag ' + escape([target_version])+ ' -m "' + message + '" ',
+      templ = gutil.template(cmd, {file:message});
+
+  exec(templ, {cwd: process.cwd()}, function(err, stdout, stderr){
+    if (err) {
+      gutil.log(err);
+      done(new Error("Tagging error", err))
+      return
+    }
+    gutil.log(stdout, stderr);
+    done();
+  });
+})
+
+
+gulp.task("reset", ['tag'], function (done) {
+  git.reset('HEAD^1', {}, done)
+})
+
+
+gulp.task('push-tags', ['reset'], function (done) {
+  git.push('origin', 'master', {args: '--tags'}, done)
+})
+
+
+module.exports = function release(version) {
+  // A.
+  fs.mkdirSync(DEST)
+  fs.writeFileSync(min_copy.dest, fs.readFileSync(min_copy.src))
+  fs.writeFileSync(debug_copy.dest, fs.readFileSync(debug_copy.src))
+
+  // set this for the gulp.task('tag', ...)
+  target_version = version;
+
+  // B.
+  gulp.start("push-tags", function () {
+    // C.
+    fs.unlinkSync(debug_copy.dest)
+    fs.unlinkSync(min_copy.dest)
+    fs.rmdirSync(DEST)
+  })
+}
