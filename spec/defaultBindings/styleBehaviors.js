cc22408cfb6fe6bc8bdbf577ea6b284caeed5ba4
Joshua Searles <bleufunk@comcast.net>
2014-03-22 00:07:28 -0400

    account for 0 value

--- spec/defaultBindings/styleBehaviors.js
+++ spec/defaultBindings/styleBehaviors.js
@@ -12,4 +12,15 @@ describe('Binding: CSS style', function() {
         myObservable(undefined);
         expect(testNode.childNodes[0].style.backgroundColor).toEqual("");
     });
+
+
+    it('Should give the element the specified CSS style value', function () {
+        var myObservable = new ko.observable(20);
+        testNode.innerHTML = "<div data-bind='style: { opacity: opacityValue }'>Hallo</div>";
+        ko.applyBindings({ opacityValue: myObservable }, testNode);
+
+        expect(+testNode.childNodes[0].style.opacity).toBe(20);
+        myObservable(0);
+        expect(+testNode.childNodes[0].style.opacity).toBe(0);
+    });
 });
--- src/binding/defaultBindings/style.js
+++ src/binding/defaultBindings/style.js
@@ -3,7 +3,8 @@ ko.bindingHandlers['style'] = {
         var value = ko.utils.unwrapObservable(valueAccessor() || {});
         ko.utils.objectForEach(value, function(styleName, styleValue) {
             styleValue = ko.utils.unwrapObservable(styleValue);
-            element.style[styleName] = styleValue || ""; // Empty string removes the value, whereas null/undefined have no effect
+            // Empty string removes the value, whereas null/undefined have no effect
+            element.style[styleName] = styleValue == null ? "" : styleValue;
         });
     }
 };
