a392c0d7b55ccbc5d916e954e28fd4a806880b36
Xavier Zwirtz <xavier@birddogsw.com>
2014-12-15 13:48:49 -0600

    Added support for deferSubscribeEvaluation to trackArrayChanges.

--- spec/computedBehaviors.js
+++ spec/computedBehaviors.js
@@ -50,4 +50,45 @@ describe('Computed', function() {
 
 
     });
+
+    it('Should not evaluate on subscribe when deferEvaluation && deferSubscribeEvaluation && trackArrayChanges', function () {
+
+        var evaluated = false;
+
+        var dependent = ko.observableArray();
+
+        var computed = ko.computed({
+            read: function () {
+                evaluated = true;
+                return dependent();
+            },
+            deferEvaluation: true,
+            deferSubscribeEvaluation: true
+        }).extend({ 'trackArrayChanges': true });
+
+        expect(evaluated).toEqual(false);
+
+        var emmitedValue;
+        computed.subscribe(function(val) {
+            emmitedValue = val;
+        }, null, 'arrayChange');
+
+        expect(evaluated).toEqual(false);
+
+        dependent.push('foo');
+
+        expect(evaluated).toEqual(false);
+
+        expect(computed()).toEqual(['foo']);
+
+        expect(evaluated).toEqual(true);
+
+        dependent.push('bar');
+        expect(emmitedValue).toEqual([
+            { status : 'added', value : 'foo', index : 0 },
+            { status : 'added', value : 'bar', index : 1 }
+        ]);
+        expect(computed()).toEqual(['foo', 'bar']);
+
+    });
 });
