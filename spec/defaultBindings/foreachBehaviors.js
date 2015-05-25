cc8a7c4dee117161501566d41e09f3f04d99e854
Michael Best <mbest@dasya.com>
2015-05-24 15:27:53 -1000

    Update foreachBehaviors.js
    fix typo

--- spec/defaultBindings/foreachBehaviors.js
+++ spec/defaultBindings/foreachBehaviors.js
@@ -243,7 +243,7 @@ describe('Binding: Foreach', function() {
         var someItems = ko.observableArray([ 'A', 'B', 'C' ]),
             callback = function(element, index, data) { if (data === 'D') throw "Exception"; };
 
-        ko.applyBindings({someItems: someItems, callback, callback });
+        ko.applyBindings({someItems: someItems, callback: callback });
         expect(testNode.childNodes[0]).toContainText('ABC');
 
         expect(function() { someItems.push('D'); }).toThrow("Exception");
