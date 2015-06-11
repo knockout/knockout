3e516ccdf7e2524afb656d133cd533078fa79ad2
zachpainter77 <zachpainter77@gmail.com>
2015-06-10 17:04:38 -0700

    Update arrayToDomNodeChildren.js

--- src/binding/editDetection/arrayToDomNodeChildren.js
+++ src/binding/editDetection/arrayToDomNodeChildren.js
@@ -118,6 +118,9 @@
         // Store a copy of the array items we just considered so we can difference it next time
         ko.utils.domData.set(domNode, lastMappingResultDomDataKey, newMappingResult);
 
+        if(options['beforeRenderAll'])
+            options['beforeRenderAll']();
+        
         // Call beforeMove first before any changes have been made to the DOM
         callCallback(options['beforeMove'], itemsForMoveCallbacks);
 
@@ -153,6 +156,9 @@
         // Finally call afterMove and afterAdd callbacks
         callCallback(options['afterMove'], itemsForMoveCallbacks);
         callCallback(options['afterAdd'], itemsForAfterAddCallbacks);
+        
+        if(options['afterRenderAll'])
+            options['afterRenderAll']();
     }
 })();
 
