cb83fc76bd08a6b8ed98199dc9633f70b8e23d99
Michael Best <mbest@dasya.com>
2015-07-28 14:15:33 -1000

    Don't check for 'require' for CommonJS. Fixes #1810

--- build/fragments/amd-pre.js
+++ build/fragments/amd-pre.js
@@ -3,7 +3,7 @@
     if (typeof define === 'function' && define['amd']) {
         // [1] AMD anonymous module
         define(['exports', 'require'], factory);
-    } else if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
+    } else if (typeof exports === 'object' && typeof module === 'object') {
         // [2] CommonJS/Node.js
         factory(module['exports'] || exports);  // module.exports is for Node.js
     } else {
