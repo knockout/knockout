2e5e93e49b434e20728e7c28a5c27a5b681d7dec
Samuel Greene <sam@samuelgreene.info>
2015-09-10 14:37:28 -0400

    Add tests for subscribe ALL

--- spec/subscribableBehaviors.js
+++ spec/subscribableBehaviors.js
@@ -153,4 +153,66 @@ describe('Subscribable', function() {
 
         expect(subscribable.customFunction).toBe(customFunction);
     });
+    
+    
+    it('Should be able to notify all values to ALL_EVENT', function() {
+        var callCounts = { test: 0, __allSubscribableEvents: 0, test2: 0 };
+        var callCount = function(type) {
+            return function() {
+                callCounts[type]++;
+            };
+        };
+        
+        var sub = new ko.subscribable();
+        sub.subscribe(callCount('test'), null, 'test');
+        sub.subscribe(callCount(ko.subscribable.fn.allEvent), null, ko.subscribable.fn.allEvent);
+        sub.subscribe(callCount('test2'), null, 'test2');
+        
+        sub.notifySubscribers('a',  'test');
+        expect(callCounts.test).toEqual(1);
+        expect(callCounts[ko.subscribable.fn.allEvent]).toEqual(1);
+        expect(callCounts.test2).toEqual(0);
+    });
+    
+    it('Should not fire double events when firing ALL_EVENT', function() {
+        var callCounts = { test: 0, __allSubscribableEvents: 0, test2: 0 };
+        var callCount = function(type) {
+            return function() {
+                callCounts[type]++;
+            };
+        };
+        
+        var sub = new ko.subscribable();
+        sub.subscribe(callCount('test'), null, 'test');
+        sub.subscribe(callCount(ko.subscribable.fn.allEvent), null, ko.subscribable.fn.allEvent);
+        sub.subscribe(callCount('test2'), null, 'test2');
+        
+        sub.notifySubscribers('a',  ko.subscribable.fn.allEvent);
+        expect(callCounts[ko.subscribable.fn.allEvent]).toEqual(1);
+    });
+
+    it('Should support subscribeAll convenience method', function() {
+        var callCounts = { test: 0, __allSubscribableEvents: 0 };
+        var callCount = function(type) {
+            return function() {
+                callCounts[type]++;
+            };
+        };
+        
+        var sub = new ko.subscribable();
+        sub.subscribe(callCount('test'), null, 'test');
+        sub.subscribeAll(callCount(ko.subscribable.fn.allEvent));
+        
+        sub.notifySubscribers('a',  ko.subscribable.fn.allEvent);
+        expect(callCounts[ko.subscribable.fn.allEvent]).toEqual(1);
+    });
+    
+    it('Should supply event type in callback when notifying allEvent', function() {
+        var sub = new ko.subscribable();
+        sub.subscribeAll(function(value, event) {
+            expect(event).toEqual('test');
+            expect(value).toEqual('a');
+        });
+        sub.notifySubscribers('a', 'test');
+    });
 });
