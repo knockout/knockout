lvsuming/Notes

When checking if an observable has a pending notification, also compare the value. If it hasn't actually changed, don't force the notification.

src/subscribables/dependentObservable.js
@@ -137,7 +137,7 @@ function computedBeginDependencyDetectionCallback(subscribable, id) {
         }
         // If the observable we've accessed has a pending notification, ensure we get notified of the actual final value (bypass equality checks)
         if (subscribable._notificationIsPending) {
-            subscribable._notifyNextChange = true;
+            subscribable._notifyNextChangeIfValueIsDifferent();
         }
     }
 }
@@ -327,10 +327,11 @@ var computedFn = {
             state.isStale = false;
         }
     },
-    peek: function () {
-        // Peek won't re-evaluate, except while the computed is sleeping or to get the initial value when "deferEvaluation" is set.
+    peek: function (evaluate) {
+        // By default, peek won't re-evaluate, except while the computed is sleeping or to get the initial value when "deferEvaluation" is set.
+        // Pass in true to evaluate if needed.
         var state = this[computedState];
-        if ((state.isStale && !state.dependenciesCount) || (state.isSleeping && this.haveDependenciesChanged())) {
+        if ((state.isStale && (evaluate || !state.dependenciesCount)) || (state.isSleeping && this.haveDependenciesChanged())) {
             this.evaluateImmediate();
         }
         return state.latestValue;

knockout/knockout

src/subscribables/dependentObservable.js
@@ -138,7 +138,7 @@ function computedBeginDependencyDetectionCallback(subscribable, id) {
         }
         // If the observable we've accessed has a pending notification, ensure we get notified of the actual final value (bypass equality checks)
         if (subscribable._notificationIsPending) {
-            subscribable._notifyNextChange = true;
+            subscribable._notifyNextChangeIfValueIsDifferent();
         }
     }
 }
@@ -333,10 +333,11 @@ var computedFn = {
             state.isStale = state.isDirty = false;
         }
     },
-    peek: function () {
-        // Peek won't re-evaluate, except while the computed is sleeping or to get the initial value when "deferEvaluation" is set.
+    peek: function (evaluate) {
+        // By default, peek won't re-evaluate, except while the computed is sleeping or to get the initial value when "deferEvaluation" is set.
+        // Pass in true to evaluate if needed.
         var state = this[computedState];
-        if ((state.isDirty && !state.dependenciesCount) || (state.isSleeping && this.haveDependenciesChanged())) {
+        if ((state.isDirty && (evaluate || !state.dependenciesCount)) || (state.isSleeping && this.haveDependenciesChanged())) {
             this.evaluateImmediate();
         }
         return state.latestValue;