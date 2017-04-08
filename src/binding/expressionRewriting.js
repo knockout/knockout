00b03e9f2979619ad75ea6c05001757179b2489f
Michael Best <mbest@dasya.com>
2017-04-07 20:14:48 -0700

    hasFocus (mixed case) also supports writing to vm properties

--- src/binding/expressionRewriting.js
+++ src/binding/expressionRewriting.js
@@ -122,7 +122,8 @@ ko.expressionRewriting = (function () {
                 if (twoWayBindings[key] && (writableVal = getWriteableValue(val))) {
                     // For two-way bindings, provide a write method in case the value
                     // isn't a writable observable.
-                    propertyAccessorResultStrings.push("'" + key + "':function(_z){" + writableVal + "=_z}");
+                    var writeKey = typeof twoWayBindings[key] == 'string' ? twoWayBindings[key] : key;
+                    propertyAccessorResultStrings.push("'" + writeKey + "':function(_z){" + writableVal + "=_z}");
                 }
             }
             // Values are wrapped in a function so that each value can be accessed independently
