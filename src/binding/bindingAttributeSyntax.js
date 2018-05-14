b623c094423b856ff36eb33f0cc4638537cf5fe6
Michael Best <mbest@dasya.com>
2018-05-14 09:41:37 -0700

    Remove variable that's no longer used.

--- src/binding/bindingAttributeSyntax.js
+++ src/binding/bindingAttributeSyntax.js
@@ -309,12 +309,10 @@
                 nextInQueue = ko.virtualElements.firstChild(elementOrVirtualElement);
             }
 
-            var bindingApplied = false;
             while (currentChild = nextInQueue) {
                 // Keep a record of the next child *before* applying bindings, in case the binding removes the current child from its position
                 nextInQueue = ko.virtualElements.nextSibling(currentChild);
                 applyBindingsToNodeAndDescendantsInternal(bindingContext, currentChild);
-                bindingApplied = true;
             }
         }
         ko.bindingEvent.notify(elementOrVirtualElement, ko.bindingEvent.childrenComplete);
