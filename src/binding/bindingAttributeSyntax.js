4d9328344647f5bb91c1a8abd0d0435c45f03cbc
Michael Best <mbest@dasya.com>
2018-09-15 20:46:54 -0700

    reference document directly instead of through window

--- src/binding/bindingAttributeSyntax.js
+++ src/binding/bindingAttributeSyntax.js
@@ -553,9 +553,9 @@
         }
 
         if (arguments.length < 2) {
-            rootNode = window.document.body;
+            rootNode = document.body;
             if (!rootNode) {
-                throw Error("ko.applyBindings: could not find window.document.body; has the document been loaded?");
+                throw Error("ko.applyBindings: could not find document.body; has the document been loaded?");
             }
         } else if (!rootNode || (rootNode.nodeType !== 1 && rootNode.nodeType !== 8)) {
             throw Error("ko.applyBindings: first parameter should be your view model; second parameter should be a DOM node");
