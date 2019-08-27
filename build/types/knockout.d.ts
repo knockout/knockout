cf7440f309d917ba8790a00d4d04f0372b7136c5
Casey Webb <notcaseywebb@gmail.com>
2019-08-27 11:49:53 -0500

    refactor: simplify Unwrapped type def

--- build/types/knockout.d.ts
+++ build/types/knockout.d.ts
@@ -245,14 +245,12 @@ export const extenders: Extenders;
 
 //#region subscribables/mappingHelpers.js
 
-type Unwrapped<T> = T extends ko.Observable<infer R>
-    ? R
-    : T extends ko.ObservableArray<infer R>
-    ? R[]
-    : T extends ko.Computed<infer R>
+type Unwrapped<T> = T extends ko.Subscribable<infer R>
     ? R
     : T extends Record<any, any>
-    ? { [P in keyof T]: Unwrapped<T[P]> }
+    ? {
+        [P in keyof T]: Unwrapped<T[P]>
+    }
     : T
 
 export function toJS<T>(rootObject: T): Unwrapped<T>;
