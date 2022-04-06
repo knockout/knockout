e765d464111c90c4e5dc9530848be39f3e626a78
Azgmohammadd <azgmohammadd@gmail.com>
2022-04-06 15:48:22 +0430

    ===

--- src/memoization.js
+++ src/memoization.js
@@ -15,7 +15,7 @@ ko.memoization = (function () {
             var memoId = ko.memoization.parseMemoText(rootNode.nodeValue);
             if (memoId != null)
                 appendToArray.push({ domNode: rootNode, memoId: memoId });
-        } else if (rootNode.nodeType == 1) {
+        } else if (rootNode.nodeType === 1) {
             for (var i = 0, childNodes = rootNode.childNodes, j = childNodes.length; i < j; i++)
                 findMemoNodes(childNodes[i], appendToArray);
         }
