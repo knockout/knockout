ff0cae56a5ec788636f0d762bf79db9d788dcc9e
abdul rawoof <abdulr@bugqore.com>
2026-07-04 14:35:08 +0530

    fix exponential backtracking in binding expression tokenizer

--- src/binding/expressionRewriting.js
+++ src/binding/expressionRewriting.js
@@ -19,16 +19,16 @@ ko.expressionRewriting = (function () {
         // Create the actual regular expression by or-ing the following regex strings. The order is important.
         bindingToken = RegExp([
             // These match strings, either with double quotes, single quotes, or backticks
-            '"(?:\\\\.|[^"])*"',
-            "'(?:\\\\.|[^'])*'",
-            "`(?:\\\\.|[^`])*`",
+            '"(?:\\\\.|[^"\\\\])*"',
+            "'(?:\\\\.|[^'\\\\])*'",
+            "`(?:\\\\.|[^`\\\\])*`",
             // Match C style comments
             "/\\*(?:[^*]|\\*+[^*/])*\\*+/",
             // Match C++ style comments
             "//.*\n",
             // Match a regular expression (text enclosed by slashes), but will also match sets of divisions
             // as a regular expression (this is handled by the parsing loop below).
-            '/(?:\\\\.|[^/])+/\w*',
+            '/(?:\\\\.|[^/\\\\])+/\w*',
             // Match text (at least two characters) that does not contain any of the above special characters,
             // although some of the special characters are allowed to start it (all but the colon and comma).
             // The text can contain spaces, but leading or trailing spaces are skipped.
